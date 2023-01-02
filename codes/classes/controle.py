4d8af3ce512e2e0d3d228cb19c1444ff259de496
Jeliel Matheus <jelielmatheus10@gmail.com>
2023-01-02 15:21:44 -0300

    Versão 2.0 completa e usando

--- codes/classes/controle.py
+++ codes/classes/controle.py
@@ -2,16 +2,19 @@ from datetime import date
 from calendar import isleap
 from tkinter import *
 from tkinter import ttk
+from tkinter.ttk import Combobox
+
 import psycopg2
 
 control_speds = Tk()
 
 class Functions():
     def limpar_tela(self):
+        self.entry_id.delete(0, END)
         self.entry_client.delete(0, END)
         self.entry_system.delete(0, END)
         self.entry_date_registro.delete(0, END)
-        self.entry_date_finish.delete(0, END)
+        self.entry_status_activity.delete(0, END)
         self.entry_observation.delete("1.0", END)
 
     def conectar_bd(self):
@@ -24,10 +27,6 @@ class Functions():
         self.cursor = self.conn.cursor()
         print("Conectado ao Banco de Dados")
 
-        # self.conn = sqlite3.connect("controle_semanal.bd")
-        # self.cursor = self.conn.cursor()
-        # print("Conectado ao Banco de Dados")
-
     def desconecta_bd(self):
         self.conn.close()
 
@@ -37,10 +36,11 @@ class Functions():
 
         self.cursor.execute("""
                         CREATE TABLE IF NOT EXISTS controle(
-                            cliente VARCHAR(200) NOT NULL PRIMARY KEY,
+                            id INTEGER PRIMARY KEY,
+                            cliente VARCHAR(200) NOT NULL,
                             sistema VARCHAR(100) NOT NULL,
                             data_registro DATE NOT NULL,
-                            data_finalizado VARCHAR(10),
+                            status BOOLEAN,
                             observacao VARCHAR(500) NOT NULL
                         );
                     """)
@@ -52,7 +52,7 @@ class Functions():
         self.list.delete(*self.list.get_children())
         self.conectar_bd()
         self.cursor.execute("""
-                            SELECT cliente, sistema, data_registro, data_finalizado, observacao 
+                            SELECT id, cliente, sistema, data_registro, status, observacao 
                             FROM controle 
                             ORDER BY sistema ASC;
                             """)
@@ -65,11 +65,18 @@ class Functions():
         for i in searchTar:
             aux = []
             for k in range(0, len(i)):
-                if k == 2:
+                if k == 3:
                     aux.append(
                         '{}/{}/{}'.format(i[k].day, i[k].month, i[k].year))
                     # print(type(i[k].day))
                     pass
+                elif k == 4:
+                    if i[k] == True:
+                        aux.append("Completa")
+                        pass
+                    else:
+                        aux.append("Incompleta")
+                        pass
                 else:
                     aux.append(i[k])
             lista_melhorada.append(aux)
@@ -87,13 +94,31 @@ class Functions():
         self.client = self.entry_client.get()
         self.system = self.entry_system.get()
         self.date_registro = self.entry_date_registro.get()
-        self.date_finish = self.entry_date_finish.get()
+        self.status = self.entry_status_activity.get()
         self.observation = self.entry_observation.get("1.0",END)
+
+        if self.status == 'Completa':
+            self.status = True
+        else:
+            self.status = False
 
         self.conectar_bd()
         self.cursor.execute("""
-                        INSERT INTO controle (cliente, sistema, data_registro, data_finalizado, observacao)
-                         VALUES (%s, %s, %s, %s, %s)""", (self.client, self.system, self.date_registro, self.date_finish, self.observation))
+                                    SELECT id, cliente, sistema, data_registro, status, observacao 
+                                    FROM controle 
+                                    ORDER BY id ASC;
+                                    """)
+        lista = self.cursor.fetchall()
+        id = 0
+
+        if len(lista) != 0:
+            for i in lista:
+                id = i[0]
+
+
+        self.cursor.execute("""
+                       INSERT INTO controle (id, cliente, sistema, data_registro, status, observacao)
+                        VALUES (%s, %s, %s, %s, %s, %s)""", (id+1, self.client, self.system, self.date_registro, self.status, self.observation))
         self.conn.commit()
         self.desconecta_bd()
         self.select_bd()
@@ -104,39 +129,47 @@ class Functions():
         self.list.selection()
 
         for n in self.list.selection():
-            col1, col2, col3, col4, col5 = self.list.item(n, 'values')
-            self.entry_client.insert(END, col1)
-            self.entry_system.insert(END, col2)
-            self.entry_date_registro.insert(END, col3)
-            self.entry_date_finish.insert(END, col4)
-            self.entry_observation.insert(END, col5)
+            col1, col2, col3, col4, col5, col6 = self.list.item(n, 'values')
+            self.entry_id.insert(END, col1)
+            self.entry_client.insert(END, col2)
+            self.entry_system.insert(END, col3)
+            self.entry_date_registro.insert(END, col4)
+            self.entry_status_activity.insert(END, col5)
+            self.entry_observation.insert(END, col6)
 
     def delete_client(self):
+        self.id = self.entry_id.get()
         self.client = self.entry_client.get()
         self.system = self.entry_system.get()
         self.date_registro = self.entry_date_registro.get()
-        self.date_finish = self.entry_date_finish.get()
+        self.status = self.entry_status_activity.get()
         self.observation = self.entry_observation.get("1.0", END)
 
 
         self.conectar_bd()
-        self.cursor.execute("""DELETE FROM controle WHERE cliente = %s """, (self.client, ))
+        self.cursor.execute("""DELETE FROM controle WHERE id = %s """, (self.id, ))
         self.conn.commit()
         self.desconecta_bd()
         self.limpar_tela()
         self.select_bd()
 
     def update_client(self):
+        self.id = self.entry_id.get()
         self.client = self.entry_client.get()
         self.system = self.entry_system.get()
         self.date_registro = self.entry_date_registro.get()
-        self.date_finish = self.entry_date_finish.get()
+        self.status = self.entry_status_activity.get()
         self.observation = self.entry_observation.get("1.0", END)
+
+        if self.status == 'Completa':
+            self.status = True
+        else:
+            self.status = False
 
         self.conectar_bd()
         self.cursor.execute("""
-                        UPDATE  controle SET sistema = %s, data_registro = %s, data_finalizado = %s, observacao = %s
-                        WHERE cliente = %s""", (self.system, self.date_registro, self.date_finish, self.observation, self.client))
+                        UPDATE  controle SET cliente = %s, sistema = %s, data_registro = %s, status = %s, observacao = %s
+                        WHERE id = %s""", (self.client, self.system, self.date_registro, self.status, self.observation, self.id))
         self.conn.commit()
         self.desconecta_bd()
         self.select_bd()
@@ -145,77 +178,24 @@ class Functions():
     def search_complete(self):
         self.conectar_bd()
         self.list.delete(*self.list.get_children())
-        self.data = 0
-        self.month = 0
-        self.year = 0
 
-        self.date_finish = self.entry_date_finish.get()
+        self.status = self.entry_status_activity.get()
 
-        self.month = self.date_finish[3:5]
-        self.year = self.date_finish[6:10]
-
-        self.data_inicial = '{}/{}/{}'.format(self.year, self.month, 1)
-        self.data_inicial = '{}/{}/{}'.format(1, self.month, self.year)
-        self.dia_fev = 0
-        self.year = int(self.year)
-        self.month = int(self.month)
-
-        if isleap(self.year):
-            self.dia_fev = 29
+        if self.status == 'Completa':
+            self.status = True
         else:
-            self.dia_fev = 28
-
-        if self.month == 1:
-            self.data_final = '{}/{}/{}'.format(31, self.month, self.year)
-        elif self.month == 2:
-            self.data_final = '{}/{}/{}'.format(self.dia_fev, self.month, self.year)
-        elif self.month == 3:
-            self.data_final = '{}/{}/{}'.format(31, self.month, self.year)
-        elif self.month == 4:
-            self.data_final = '{}/{}/{}'.format(30, self.month, self.year)
-        elif self.month == 5:
-            self.data_final = '{}/{}/{}'.format(31, self.month, self.year)
-        elif self.month == 6:
-            self.data_final = '{}/{}/{}'.format(30, self.month, self.year)
-        elif self.month == 7:
-            self.data_final = '{}/{}/{}'.format(31, self.month, self.year)
-        elif self.month == 8:
-            self.data_final = '{}/{}/{}'.format(31, self.month, self.year)
-        elif self.month == 9:
-            self.data_final = '{}/{}/{}'.format(30, self.month, self.year)
-        elif self.month == 10:
-            self.data_final = '{}/{}/{}'.format(31, self.month, self.year)
-        elif self.month == 11:
-            self.data_final = '{}/{}/{}'.format(30, self.month, self.year)
-        else:
-            self.data_final = '{}/{}/{}'.format(31, self.month, self.year)
-
+            self.status = False
 
         self.cursor.execute("""
-                        SELECT cliente, sistema, data_registro, data_finalizado, observacao 
+                        SELECT id, cliente, sistema, data_registro, status, observacao 
                         FROM controle
-                        WHERE data_finalizado BETWEEN %s and %s
+                        WHERE status = TRUE
                         ORDER BY cliente ASC
-                        """, (self.data_inicial, self.data_final))
+                        """, (self.status))
         lista = self.cursor.fetchall()
         print(lista)
-        lista_melhorada2 = []
-        for i in lista:
-            aux = []
-            for k in range(0, len(i)):
-                if k == 2:
-                    aux.append(
-                        '{}/{}/{}'.format(i[k].day, i[k].month, i[k].year))
-                    # print(type(i[k].day))
-                    pass
-                if k == 3:
-                    aux.append(i[k])
-                    pass
-                else:
-                    aux.append(i[k])
-            lista_melhorada2.append(aux)
 
-        for i in lista_melhorada2:
+        for i in lista:
             self.list.insert("", END, values=i)
         self.limpar_tela()
         self.desconecta_bd()
@@ -223,76 +203,16 @@ class Functions():
     def search_incomplete(self):
         self.conectar_bd()
         self.list.delete(*self.list.get_children())
-        self.data = 0
-        self.month = 0
-        self.year = 0
 
-        # self.date_registro = self.entry_date_registro.get()
-        #
-        # self.month = self.date_registro[3:5]
-        # self.year = self.date_registro[6:10]
-        #
-        # self.data_inicial = '{}/{}/{}'.format(self.year, self.month, 1)
-        # self.data_inicial = '{}/{}/{}'.format(1, self.month, self.year)
-        # self.dia_fev = 0
-        # self.year = int(self.year)
-        # self.month = int(self.month)
-        #
-        # if isleap(self.year):
-        #     self.dia_fev = 29
-        # else:
-        #     self.dia_fev = 28
-        #
-        # if self.month == 1:
-        #     self.data_final = '{}/{}/{}'.format(31, self.month, self.year)
-        # elif self.month == 2:
-        #     self.data_final = '{}/{}/{}'.format(self.dia_fev, self.month, self.year)
-        # elif self.month == 3:
-        #     self.data_final = '{}/{}/{}'.format(31, self.month, self.year)
-        # elif self.month == 4:
-        #     self.data_final = '{}/{}/{}'.format(30, self.month, self.year)
-        # elif self.month == 5:
-        #     self.data_final = '{}/{}/{}'.format(31, self.month, self.year)
-        # elif self.month == 6:
-        #     self.data_final = '{}/{}/{}'.format(30, self.month, self.year)
-        # elif self.month == 7:
-        #     self.data_final = '{}/{}/{}'.format(31, self.month, self.year)
-        # elif self.month == 8:
-        #     self.data_final = '{}/{}/{}'.format(31, self.month, self.year)
-        # elif self.month == 9:
-        #     self.data_final = '{}/{}/{}'.format(30, self.month, self.year)
-        # elif self.month == 10:
-        #     self.data_final = '{}/{}/{}'.format(31, self.month, self.year)
-        # elif self.month == 11:
-        #     self.data_final = '{}/{}/{}'.format(30, self.month, self.year)
-        # else:
-        #     self.data_final = '{}/{}/{}'.format(31, self.month, self.year)
-
-
-        data_incomplete = '{}/{}/{}'.format(1010,10,10)
-        data_incomplete2 = '{}/{}/{}'.format(1010,10,10)
-        print(data_incomplete)
         self.cursor.execute("""
-                                SELECT cliente, sistema, data_registro, data_finalizado, observacao 
+                                SELECT id, cliente, sistema, data_registro, status, observacao 
                                 FROM controle
-                                WHERE data_finalizado BETWEEN %s AND %s
+                                WHERE status = False
                                 ORDER BY cliente ASC
-                                """, (data_incomplete, data_incomplete2))
+                                """, (self.status))
         lista = self.cursor.fetchall()
-        lista_melhorada3 = []
-        for i in lista:
-            aux = []
-            for k in range(0, len(i)):
-                if k == 2 or k == 3:
-                    aux.append(
-                        '{}/{}/{}'.format(i[k].day, i[k].month, i[k].year))
-                    # print(type(i[k].day))
-                    pass
-                else:
-                    aux.append(i[k])
-            lista_melhorada3.append(aux)
 
-        for i in lista_melhorada3:
+        for i in lista:
             self.list.insert("", END, values=i)
         self.limpar_tela()
         self.desconecta_bd()
@@ -471,34 +391,38 @@ class Controler(Functions):
         self.lb_title = Label(self.frame_information, text= "CONTROLE SEMANAL", font= "-weight bold -size 15", bg= self.color_frames, fg= self.color_names)
         self.lb_title.place(rely= 0.01, relx= 0.32, relwidth= 0.3, relheight= 0.2)
 
+        self.lb_id = Label(self.frame_information, text= "Id", font= "-weight bold -size 10", bg= self.color_frames, fg= self.color_names)
+        self.lb_id.place(rely= 0.08, relx= 0.01, relwidth= 0.015, relheight= 0.1)
+
+        self.entry_id = Entry(self.frame_information)
+        self.entry_id.place(rely= 0.2, relx= 0.01, relwidth= 0.025)
+
         self.lb_client = Label(self.frame_information, text= "Cliente", font= "-weight bold -size 10", bg= self.color_frames, fg= self.color_names)
-        self.lb_client.place(rely= 0.08, relx= 0.01, relwidth= 0.065, relheight= 0.1)
+        self.lb_client.place(rely= 0.08, relx= 0.05, relwidth= 0.065, relheight= 0.1)
 
         self.entry_client = Entry(self.frame_information)
-        self.entry_client.place(rely= 0.2, relx= 0.01, relwidth= 0.13)
+        self.entry_client.place(rely= 0.2, relx= 0.052, relwidth= 0.13)
 
 
         self.lb_system = Label(self.frame_information, text= "Sistema", font= "-weight bold -size 10", bg= self.color_frames, fg= self.color_names)
-        self.lb_system.place(rely= 0.08, relx= 0.17, relwidth= 0.073, relheight= 0.1)
+        self.lb_system.place(rely= 0.08, relx= 0.215, relwidth= 0.073, relheight= 0.1)
 
         self.entry_system = Entry(self.frame_information)
-        self.entry_system.place(rely= 0.2, relx= 0.17, relwidth=0.08)
+        self.entry_system.place(rely= 0.2, relx= 0.215, relwidth=0.08)
 
 
         self.lb_date_registro = Label(self.frame_information, text= "Data Registro", font= "-weight bold -size 10", bg= self.color_frames, fg= self.color_names)
-        self.lb_date_registro.place(rely= 0.4, relx= 0.01, relwidth= 0.122, relheight= 0.1)
+        self.lb_date_registro.place(rely= 0.4, relx= 0.05, relwidth= 0.122, relheight= 0.1)
 
         self.entry_date_registro = Entry(self.frame_information)
-        self.entry_date_registro.place(rely= 0.52, relx= 0.01, relwidth= 0.122)
+        self.entry_date_registro.place(rely= 0.52, relx= 0.052, relwidth= 0.122)
         self.entry_date_registro.bind("<KeyRelease>", self.format_date_registro)
 
+        self.lb_status_activity = Label(self.frame_information, text= "Status Atividade", font= "-weight bold -size 10", bg= self.color_frames, fg= self.color_names)
+        self.lb_status_activity.place(rely= 0.4, relx= 0.215, relwidth= 0.145, relheight= 0.1)
 
-        self.lb_date_finish = Label(self.frame_information, text= "Data Finalizado", font= "-weight bold -size 10", bg= self.color_frames, fg= self.color_names)
-        self.lb_date_finish.place(rely= 0.4, relx= 0.17, relwidth= 0.14, relheight= 0.1)
-
-        self.entry_date_finish = Entry(self.frame_information)
-        self.entry_date_finish.place(rely= 0.52, relx= 0.17, relwidth= 0.122)
-        self.entry_date_finish.bind("<KeyRelease>", self.format_date_finish)
+        self.entry_status_activity = Combobox(self.frame_information, values=['Completa', 'Incompleta'])
+        self.entry_status_activity.place(rely= 0.52, relx= 0.215, relwidth= 0.122)
 
 
         self.lb_observation = Label(self.frame_information, text= "Anotações", font= "-weight bold -size 10", bg= self.color_frames, fg= self.color_names)
@@ -548,21 +472,23 @@ class Controler(Functions):
         self.bt_exit.place(rely= 0.72, relx= 0.335, relwidth= 0.075, relheight= 0.2)
 
     def frame_list_activity(self):
-        self.list = ttk.Treeview(self.frame_activity, height= 3, columns= ("col1", "col2", "col3", "col4", "col5"))
+        self.list = ttk.Treeview(self.frame_activity, height= 3, columns= ("col1", "col2", "col3", "col4", "col5", "col6"))
 
         self.list.heading("#0", text= "")
-        self.list.heading("#1", text= "Cliente")
-        self.list.heading("#2", text= "Sistema")
-        self.list.heading("#3", text= "Data Registro")
-        self.list.heading("#4", text= "Data Finalizado")
-        self.list.heading("#5", text= "Anotações")
+        self.list.heading("#1", text= "Id")
+        self.list.heading("#2", text= "Cliente")
+        self.list.heading("#3", text= "Sistema")
+        self.list.heading("#4", text= "Data Registro")
+        self.list.heading("#5", text= "Status")
+        self.list.heading("#6", text= "Anotações")
 
         self.list.column("#0", width=1)
-        self.list.column("#1", width= 50)
-        self.list.column("#2", width= 30)
-        self.list.column("#3", width= 47)
-        self.list.column("#4", width= 53)
-        self.list.column("#5", width= 310)
+        self.list.column("#1", width= 8)
+        self.list.column("#2", width= 45)
+        self.list.column("#3", width= 32)
+        self.list.column("#4", width= 33)
+        self.list.column("#5", width= 38)
+        self.list.column("#6", width= 310)
 
         self.list.place(rely= 0.01, relx= 0.01, relwidth= 0.96, relheight= 0.98)
 
